b6a9d194f8deecb7303db82140fbe6e5da4b52c0
Blessing C Nwobodo <nwobodoblessingc@gmail.com>
2025-11-17 08:22:27 +0100

    Added new files and modified others

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import ProfilePage from './components/ProfilePage';
+import { UserContext } from './UserContext'; // <-- fixed path
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
